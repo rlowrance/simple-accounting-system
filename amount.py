da8b69323e546ae02a84b35f263359fcd3e00285
Roy Lowrance <roy.lowrance@gmail.com>
2025-01-04 17:55:04 -0500

    add functions add, subtract, greater

--- amount.py
+++ amount.py
@@ -11,20 +11,69 @@ def make(dollars = None, cents = None) -> Amount:
     assert cents >= 0
     return Amount(dollars, cents)
 
-def add_to_amount(lhs, rhs) -> Amount:
+def add(lhs, rhs) -> Amount:
     assert isinstance(lhs, Amount)
     assert isinstance(rhs, Amount)
-    return _normalize(make(dollars = lhs.dollars + rhs.dollars,
-                           cents = lhs.cents + rhs.cents))
+    return _normalize(Amount(lhs.dollars + rhs.dollars, lhs.cents + rhs.cents))
 
-def _normalize(amount) -> Amount:
-    assert isinstance(amount, Amount)
-    if amount.cents > 100:
-        return _normalize(make(dollars = amount.dollars + 1,
-                               cents = amount.cents - 100))
-    return amount
+def subtract(lhs, rhs) -> Amount:
+    assert isinstance(lhs, Amount)
+    assert isinstance(rhs, Amount)
+    return _normalize(Amount(lhs.dollars - rhs.dollars, lhs.cents - rhs.cents))
+
+def greater(lhs, rhs) -> bool:
+    assert isinstance(lhs, Amount)
+    assert isinstance(rhs, Amount)
+    if lhs.dollars > rhs.dollars:
+        return True
+    if lhs.dollars == rhs.dollars and lhs.cents > rhs.cents:
+        return True
+    return False
+
+def _normalize(lhs) -> Amount:
+    assert isinstance(lhs, Amount)
+    if lhs.cents > 99:
+        return _normalize(Amount(lhs.dollars + 1, lhs.cents - 100))
+    if lhs.cents < 0:
+        return _normalize(Amount(lhs.dollars - 1, lhs.cents + 100))
+    return lhs
 
 class Test(unittest.TestCase):
+    def test_normalize(self):
+        tests = (
+            ((100, 10), (100, 10)),
+            ((-100, 10), (-100, 10)),
+            ((100, 101), (101, 1)),
+            ((100, 100), (101, 0)),
+            ((-1, -10), (-2, 90)),
+        )
+        for test in tests:
+            a, b = test
+            x = Amount(a[0], a[1])
+            expected = Amount(b[0], b[1])
+            r = _normalize(x)
+            self.assertEqual(expected, r)
+
+    def test_subtract(self):
+        tests = (
+            ((100, 10), (1, 2), (99, 8)),
+            ((100, 10), (101, 8), (-1, 2)),
+            ((100, 10), (201, 12), (-102, 8))
+        )
+
+    def test_greater(self):
+        tests = (
+            ((100, 10), (101, 12), False),
+            ((100, 10), (100, 9), True),
+            ((100, 10), (100, 10), False),
+        )
+        for test in tests:
+            a, b, expected = test
+            def _make(x): return Amount(x[0], x[1])
+            x = _make(a)
+            y = _make(b)
+            self.assertEqual(expected, greater(x, y))
+
     def test_make(self):
         tests = (
             (None, 50, 0, 50),
@@ -37,16 +86,19 @@ class Test(unittest.TestCase):
             self.assertEqual(amount.dollars, expected_d)
             self.assertEqual(amount.cents, expected_c)
 
-    def test_add_to_amount(self):
+    def test_add(self):
         tests = (
-            (100, 99, 0, 2, 101, 1),
+            ((100, 99), (0, 0), (100, 99)),
+            ((100, 99), (0, 1), (101, 0)),
+            ((100, 99), (0, 2), (101, 1)),
         )
         for test in tests:
-            xd, xc, yd, yc, ed, ec = test
-            x = make(xd, xc)
-            y = make(yd, yc)
-            expected = make(ed, ec)
-            self.assertEqual(expected, add_to_amount(x, y))
+            a, b, c = test
+            def _make(x): return Amount(x[0], x[1])
+            x = _make(a)
+            y = _make(b)
+            expected = _make(c)
+            self.assertEqual(expected, add(x, y))
 
 if __name__ == "__main__":
     unittest.main()
