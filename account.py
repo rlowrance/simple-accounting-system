c234bab7bdc374b00d26a413db0037dade985305
Roy Lowrance <roy.lowrance@gmail.com>
2025-01-04 17:54:09 -0500

    make uses correct attribute names

--- account.py
+++ account.py
@@ -7,13 +7,13 @@ Account = collections.namedtuple('Account', 'category name')
 
 _allowed_account_categories = {"Asset", "Liability", "Equity", "Revenue", "Expense"}
 
-def make(category_name = None, account_name = None) -> Account:
-    category_name_capitalized = category_name.capitalize()
+def make(category = None, name = None) -> Account:
+    category_name_capitalized = category.capitalize()
     if category_name_capitalized not in _allowed_account_categories:
-        raise AccountingSystemError(f"invalid account category: {category_name}")
-    if not isinstance(account_name, str):
-        raise AccountingSystemError(f"account name not a string: found: {account_name}")
-    account_name_splits = tuple(account_name.split())
+        raise AccountingSystemError(f"invalid account category: {category}")
+    if not isinstance(name, str):
+        raise AccountingSystemError(f"account name not a string: found: {name}")
+    account_name_splits = tuple(name.split())
     return Account(category_name_capitalized, account_name_splits)
 
 class Test(unittest.TestCase):
