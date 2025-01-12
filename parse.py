43d6cf705e9e52c1f0bf7c299588f70899316205
Roy Lowrance <roy.lowrance@gmail.com>
2025-01-12 08:00:56 -0500

    move parsing code to parse.py

---
+++ parse.py
@@ -0,0 +1,170 @@
+# parse input file lines into acount declarations and journal entries
+
+import copy
+import dataclasses
+import unittest
+
+from dataclasses import dataclass
+from typing import Union
+
+# ref: https://stackoverflow.com/questions/4071396/how-to-split-by-comma-and-strip-white-spaces-in-python
+def split_and_strip(s, splitter=None) -> list:
+    if splitter is None:
+        return list(map(str.strip, s.split()))
+    return list(map(str.strip, s.split(splitter)))
+
+@dataclass(frozen=True)
+class DateComponents:
+    year: Union[None, int] = None
+    month: Union[None, int] = None
+    day: Union[None, int] = None
+
+    @classmethod
+    def from_string(cls, s: str) -> Self:
+        if len(s) == 8: return DateComponents(year=int(s[0:4]), month=int(s[4:6]), day=int(s[6:8]))
+        if len(s) == 4: return DateComponents(month=int(s[0:2]), day=int(s[2:4]))
+        if len(s) == 2: return DateComponents(day=int(s))
+        if len(s) == 0: return DateComponents()
+        raise ValueError(f'date {s} is not of form YYYYMMDD or MMDD or DD')
+
+
+    def is_complete(self) -> bool:
+        return self.year is not None and self.month is not None and self.day is not None
+
+    def to_date(self) -> datetime.date:
+        assert isinstance(self.year, int)
+        assert isinstance(self.month, int)
+        assert isinstance(self.day, int)
+        return datetime.date(self.year, self.month, self.day)
+
+    def replace(**kwargs) -> Self:
+        return dataclasses.replace(self, **kwargs)
+
+def make_amount(s: str) -> Amount:
+    splits = split_and_strip(s, ".")
+    if len(splits) == 1: # ex: 123
+        dollars = safe_cast(splits[0], int)
+        if dollars is None: raise ValueError(f'dollar amount {splits[0]} is not an int')
+        return Amount(dollars=dollars, cents=0)
+    if len(splits) == 2: # ex: 123.45
+        a, b = splits
+        dollars = safe_cast(a, int)
+        cents = safe_cast(b, int)
+        if dollars is None:
+            raise ValueError(f'dollars amount {a} is not an int')
+        if cents is None:
+            raise ValueError(f'cents amount {b} is not an int')
+        return Amount(dollars=dollars, cents=cents)
+    raise ValueError(f's {s} is not like 123.45 (specifying dollars and cents)')
+
+def parse_account_declaration(s: str) -> AccountDeclaration:
+    splits = split_and_strip(s, splitter=':')
+    if len(splits) == 2: return AccountDeclaration(category=splits[0], name=splits[1])
+    raise ValueError(f'account declaration not like Asset:cash; found: {s}')
+
+def make_journal_entry(splits: List[str]) -> JournalEntry:
+    assert len(splits) == 5
+    return JournalEntry(
+        date=datetime.date.fromisoformat(splits[0]),
+        amount=make_amount(splits[1]),
+        debit_account=splits[2],
+        credit_account=splits[3],
+        description=splits[4],
+        source='',
+        source_location=''
+    )
+    # synthesize missing splits
+    new_splits = copy.deepcopy(splits)
+    new_splits.append('')
+    return make_journal_entry(new_splits)
+
+def parse(line: Line, last_journal_entry: Union[JournalEntry, None]) -> Union[AccountDeclaration, JournalEntry]:
+    def complete(je: JournalEntry) -> JournalEntry:
+        return dataclasses.replace(je, source=line.source, source_location=line.source_location)
+    splits = csv_line_to_str(line.text)  # allow quoting and other CSV file layout conventions
+    if len(splits) == 1: 
+        return parse_account_declaration(line.text)
+    else:
+        while len(splits) < 5:
+            splits.append('')
+        date_s, amount_s, debit_account_s, credit_account_s, description_s = splits
+        date_components = DateComponents.from_string(date_s)
+        if not date_components.is_complete():
+            if last_journal_entry is None:
+                raise ValueError(f'invalid date {date_s}')
+            def fill(x, y): return y if x is None else x
+            date_components = dataclasses.replace(
+                date_components,
+                year=fill(date_components.year, last_journal_entry.date.year),
+                month=fill(date_components.month, last_journal_entry.date.month),
+                day=fill(date_components.day, last_journal_entry.date.day)
+            )
+        date = date_components.to_date()
+
+        if last_journal_entry is None:
+            return JournalEntry(
+                date=date,
+                amount=make_amount(amount_s),
+                debit_account=debit_account_s,
+                credit_account=credit_account_s,
+                description=description_s,
+                source=line.source,
+                source_location=line.source_location
+            )
+        else:
+            return JournalEntry(
+                date=date,
+                amount=make_amount(amount_s) if len(amount_s) > 0 else last_journal_entry.amount,
+                debit_account=debit_account_s if len(debit_account_s) > 0 else last_journal_entry.debit_account,
+                credit_account=credit_account_s if len(credit_account_s) > 0 else last_journal_entry.credit_account,
+                description=description_s if len(description_s) >0 else last_journal_entry.description,
+                source=line.source,
+                source_location=line.source_location
+            )
+
+class Test(unittest.TestCase):
+    def test_parse_journal_entry_line(self):
+        expected = JournalEntry(
+            date=datetime.date(2025,12,25),
+            amount=Amount(dollars=123, cents=45),
+            debit_account='cash',
+            credit_account='owners equity',
+            description='description',
+            source='source',
+            source_location='source_location')
+        tests = (
+            ',123.45',
+            ',,,,',
+            ',,,',
+            '25,',
+            '1225,',
+            ',123.45',
+            ',,cash',
+            ',,, owners equity',
+            ',,,owners equity   ,',
+            ',,,,description    ',
+        )
+        for test in tests:
+            line = Line(test, source='source', source_location='source_location')
+            r = parse(line=line, last_journal_entry=expected)
+            self.assertTrue(isinstance(r, JournalEntry))
+            assert isinstance(r, JournalEntry)
+            self.assertEqual(expected.date, r.date)
+            self.assertEqual(expected.amount, r.amount)
+            self.assertEqual(expected.debit_account, r.debit_account)
+            self.assertEqual(expected.credit_account, r.credit_account)
+            self.assertEqual(expected.description, r.description)
+            self.assertEqual(expected.source, r.source)
+            self.assertEqual(expected.source_location, r.source_location)
+
+    def test_parse_csv_line(self):
+        tests = (
+            ('1, 2, 3', ['1', '2', '3']),
+            ('1,    2  ,     3', ['1', '2', '3']),
+        )
+        for test in tests:
+            x, expected = test
+            self.assertEqual(expected, csv_line_to_str(x))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ sac.py
@@ -0,0 +1,123 @@
+# simply accounting system (version 2)
+# Read a directory of text files containing account declarations and journal entries. Skip certain files including those whose name start with "_".
+# Write a directory named _{datetime}-summary containing CSV files that balances, ledgers, an income statement, and a balance sheet.
+import copy
+import csv
+import dataclasses
+from dataclasses import dataclass
+import datetime
+import io
+from typing import Dict, List, Self, Union
+import os
+import unittest
+
+from accountdeclaration import AccountDeclaration
+from accountingsystem import AccountingSystem
+from amount import Amount
+from columnsreport import ColumnsReport
+from filereport import FileReport
+from journalentry import JournalEntry
+from line import Line
+from reportcolumn import ReportColumn
+
+# ref: https://stackoverflow.com/questions/3305926/python-csv-string-to-array
+def csv_line_to_str(line: str) -> List[str]:
+    return list(map(str.strip, next(csv.reader([line]))))
+
+# ref: https://stackoverflow.com/questions/3305926/python-csv-string-to-array
+def csv_line_from_row(row: List[str]) -> str:
+    with io.StringIO() as line:
+        csv.writer(line).writerow(row)
+        return line.getvalue().strip()
+
+# ref: https://stackoverflow.com/questions/6330071/safe-casting-in-python
+def safe_cast(value, to_type, default_value=None):
+    try:
+        return to_type(value)
+    except (ValueError, TypeError):
+        return default_value
+
+
+
+# process files in a directory
+def make_accounting_system(directory='.') -> AccountingSystem:
+    r = AccountingSystem.empty()
+    for objname in os.listdir(directory):
+        if objname.startswith('.') or objname.startswith('_') or objname.endswith('.py'):
+            print(f'skipping {objname}')
+            continue
+        if not (objname.endswith('.txt') or objname.endswith('.csv')):
+            print('skipping {objname}')
+            continue
+        path = os.path.join(directory, objname)
+        if not os.path.isfile(path):
+            print('skipping directory {filename}')
+        
+        print(f'reading file {objname}')
+        with open(path, 'r') as file:
+            content = file.read()
+            last_journal_entry = None
+            file_report = FileReport()
+            for line_index, line in enumerate(content.split('\n')):
+                print(line)
+                line = line.strip()
+                if line.startswith('#'): continue
+                if len(line) == 0: continue
+                if line.isspace(): continue
+                command = parse(
+                    line=Line(text=line, source=objname, source_location=f'line {line_index+1}'),
+                    last_journal_entry=last_journal_entry
+                )
+                if isinstance(command, JournalEntry):
+                    last_journal_entry = command
+                r = r.join(command)
+                fileReport = fileReport.join(command)
+            for report_line in fileReport.render():
+                print(line)
+    return r
+
+def make_reports(accounting_system: AccountingSystem) -> Dict[str, ColumnsReport]:
+    r = {}
+    r['balances'] = make_report_balances(accounting_system)
+    return r
+
+def account_name_for_sorting(s: str) -> List[str]:
+    return split_and_strip(s)
+
+# category account balance
+def make_report_balances(accounting_system) -> ColumnsReport:
+    category_column = []
+    account_column = []
+    balance_column = []
+    for category in AccountDeclaration.allowed_account_categories():
+        account_names_for_category = filter(lambda account_name: accounting_system.category_for[account_name] == category, accounting_system.balances.keys())
+        sorted_account_name_for_category = sorted(account_names_for_category, key=account_name_for_sorting)
+        for account_name in sorted_account_name_for_category:
+            category_column.append(category)
+            account_column.append(account_name)
+            balance_column.append(accounting_system.balances[account_name])
+    r = ColumnsReport()
+    r = r.join(ReportColumn(alignment='left', items=category_column))
+    r = r.join(ReportColumn(alignment='left', items=account_column))
+    r = r.join(ReportColumn(alignment='right', items=balance_column))
+    return r
+
+def main():
+    directory = '.'
+    accounting_system = make_accounting_system(directory)
+    for report_name, report in make_reports(accounting_system).items():
+        print(f'report name: {report_name}')
+        for report_line in report.render():
+            print(f'  {report_line}')
+
+
+
+if __name__ == '__main__':
+    #unittest.main()
+    main()
+
+
+
+
+
+
