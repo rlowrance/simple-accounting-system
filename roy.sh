d150075889761300c3e8dbd1edb0aae04c8ab073
Roy Lowrance <roy.lowrance@gmail.com>
2025-01-22 09:08:56 -0500

    also run balances.py

--- roy.sh
+++ roy.sh
@@ -1,2 +1,3 @@
 # run accounting for all of Roy's accounts
-python3 ledgers.py roy-accounts.csv roy-*.csv
+python3 ledgers.py roy-accounts.csv roy-*.csv >_roy.ledgers.csv
+python3 balances.py _roy.ledgers.csv
