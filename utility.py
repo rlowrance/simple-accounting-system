76ec516aa3d625ae85a4fe45a7a25a1ae9dca748
Roy Lowrance <roy.lowrance@gmail.com>
2025-01-22 08:01:22 -0500

    add is_sequence(), Maybe monad

--- utility.py
+++ utility.py
@@ -29,6 +29,15 @@ def invert_dict(d: Dict[Any, Any]) -> Dict[Any, Set]:
         r.setdefault(v, set()).add(k)
     return r
 
+# ref: https://stackoverflow.com/questions/2937114/python-check-if-an-object-is-a-sequence
+def is_sequence(obj) -> bool:
+    try:
+        len(obj)
+        obj[0:0]
+        return True
+    except TypeError:
+        return False
+
 # ref: https://stackoverflow.com/questions/6330071/safe-casting-in-python
 def safe_cast(value, to_type, default_value=None):
     try:
@@ -43,6 +52,13 @@ def split_and_strip(s, splitter=None) -> List:
     return list(map(str.strip, s.split(splitter)))
 
 
+class Maybe: # ref https://dev.to/nhradek/monads-in-python-4npa
+    def __init__(self, value): self.value = value
+    def __repr__(self): return f'Maybe({self.value})'
+    def unwrap(self): return self.value
+    def bind(self, func): return Maybe(None) if self.value is None else Maybe(func(self.value))
+    @staticmethod
+    def wrap(value): return Maybe(value)
 
 class Test(unittest.TestCase):
     def test_csv_line_to_str(self):
